a301272d391004a2c629219c1450449b08dfe817
Raizend <condehalo117@gmail.com>
2021-09-23 10:50:11 -0500

    creacion de repositorio

---
+++ src/views/Login.vue
@@ -0,0 +1,33 @@
+<template>
+    <v-card id="card-login" class="mx-auto" max-width="344">
+    <v-card-text>
+        <p class="text-h4 text--primary">LOGIN</p>
+        <v-form>
+        <v-text-field
+            label="usuario"
+            placeholder="usuario"
+            outlined
+        ></v-text-field>
+        <v-text-field
+            label="contraseña"
+            placeholder="contraseña"
+            outlined
+        ></v-text-field>
+        </v-form>
+    </v-card-text>
+    <v-card-actions>
+        <v-btn text color="grey"> registrar </v-btn>
+        <v-btn text color="black"> iniciar sesión </v-btn>
+    </v-card-actions>
+    </v-card>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+#card-login{
+    margin-top: 2rem;
+}
+</style>
