f809b13fb4ee7b7c23f0f77c4dbcd9663e48ce0f
mauro1777 <mauro.12arango@hotmail.com>
2021-09-27 02:16:04 -0500

    Formulario de registro

--- src/components/Certificado.vue
+++ src/components/Certificado.vue
@@ -4,7 +4,16 @@
       v-model="name"
       :error-messages="nameErrors"
       :counter="25"
-      label="Nombre"
+      label="Registrador"
+      required
+      @input="$v.name.$touch()"
+      @blur="$v.name.$touch()"
+    ></v-text-field>
+    <v-text-field
+      v-model="name2"
+      :error-messages="name2Errors"
+      :counter="25"
+      label="Nombre de la mascota"
       required
       @input="$v.name.$touch()"
       @blur="$v.name.$touch()"
@@ -12,7 +21,7 @@
     <v-text-field
       v-model="email"
       :error-messages="emailErrors"
-      label="E-mail"
+      label="E-mail del dueño de la mascota"
       required
       @input="$v.email.$touch()"
       @blur="$v.email.$touch()"
@@ -26,11 +35,19 @@
       @change="$v.select.$touch()"
       @blur="$v.select.$touch()"
     ></v-select>
+    <v-select
+      v-model="select2"
+      :items2="items2"
+      :error-messages="select2Errors"
+      label="Género"
+      required
+      @change="$v.select2.$touch()"
+      @blur="$v.select2.$touch()"
+    ></v-select>
     <v-text-field
       v-model="municipio"
-      :error-messages="municipioErrors"
       :counter="30"
-      label="Municipio"
+      label="Vacuna"
       required
       @input="$v.municipio.$touch()"
       @blur="$v.municipio.$touch()"
@@ -38,7 +55,7 @@
     <v-checkbox
       v-model="checkbox"
       :error-messages="checkboxErrors"
-      label="Estas de acuerdo con está información suministrada ?"
+      label="¿Estas de acuerdo con está información suministrada ?"
       required
       @change="$v.checkbox.$touch()"
       @blur="$v.checkbox.$touch()"
@@ -60,7 +77,7 @@ export default {
     name: { required, maxLength: maxLength(25) },
     email: { required, email },
     select: { required },
-    municpio:{required, maxLength: maxLength(30)},
+    select2: { required },
     checkbox: {
       checked(val) {
         return val;
@@ -73,6 +90,7 @@ export default {
     email: "",
     select: null,
     items: ["Perro", "Gato", "Otro"],
+    items2: ["Perro", "Gato", "Otro"],
     municpio: null,
     checkbox: false,
   }),
@@ -91,6 +109,12 @@ export default {
       !this.$v.select.required && errors.push("Mascota es requerido");
       return errors;
     },
+    select2Errors() {
+      const errors = [];
+      if (!this.$v.select2.$dirty) return errors;
+      !this.$v.select2.required && errors.push("Género es requerido");
+      return errors;
+    },
     nameErrors() {
       const errors = [];
       if (!this.$v.name.$dirty) return errors;
@@ -99,6 +123,15 @@ export default {
       !this.$v.name.required && errors.push("Se requiere un nombre");
       return errors;
     },
+    /*
+    name2Errors() {
+      const errors = [];
+      if (!this.$v.name.$dirty) return errors;
+      !this.$v.name.maxLength &&
+        errors.push("El nombre no debe de superar 25 caracteres");
+      !this.$v.name.required && errors.push("Se requiere un nombre");
+      return errors;
+    },*/
     emailErrors() {
       const errors = [];
       if (!this.$v.email.$dirty) return errors;
@@ -106,6 +139,7 @@ export default {
       !this.$v.email.required && errors.push("E-mail is required");
       return errors;
     },
+    /*
     municipioErrors() {
       const errors = [];
       if (!this.$v.name.$dirty) return errors;
@@ -113,7 +147,7 @@ export default {
         errors.push("El municipio no debe de superar 30 caracteres");
       !this.$v.municipio.required && errors.push("Se requiere un Municipio");
       return errors;
-    },
+    },*/
   },
 
   methods: {
@@ -123,19 +157,21 @@ export default {
     clear() {
       this.$v.$reset();
       this.name = "";
+      this.name2 = "";
       this.email = "";
       this.select = null;
+      this.select2 = null;
       this.checkbox = false;
-      this.municpio=null;
+      this.municpio = "";
     },
   },
 };
 </script>
 
 <style>
-#Tabla{
-    margin-left: 20vw;
-    margin-right: 20vw;
-    
+#Tabla {
+  margin-left: 20vw;
+  margin-right: 20vw;
+  margin-bottom: 4vh;
 }
 </style>
