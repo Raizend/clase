7bee33afec99a63fc1fee3919f271c56d2ab4c05
Nelson Herrera Castro <nelsonehc@gmail.com>
2021-10-08 02:54:42 -0500

    Actualizacion de home, y componentes

--- src/components/Peipagina.vue
+++ src/components/Peipagina.vue
@@ -1,52 +1,28 @@
 <template>
     <v-container>
-        <v-footer
-            dark
-            padless id="footer">
-            <v-card
-                class="flex"
-                flat
-                tile
-            >
-                <v-card-title class="teal">
-                <article>
-                <v-card-text>
-                    <h3>Nosotros</h3>
-                    <p>+57 5 6900212 | + 57 5 6901112</p>
-                    <p>+57 3136224678 | +57 3153452877</p>
-                    <p>contactenos@mivacuna.com.co</p>
-                </v-card-text>
+        <div id="footer">
+            <article>
+                <h2 ><u class="footer-section_title">Nosotros</u></h2>
+                <p>+57 5 6900212 | + 57 5 6901112</p>
+                <p>+57 3136224678 | +57 3153452877</p>
+                <p>contactenos@mivacuna.com.co</p>
+                <img src="https://cdn-icons-png.flaticon.com/128/174/174879.png" alt="iconoWatsapp" width="30" height="30"><img src="
+                https://cdn-icons-png.flaticon.com/128/174/174848.png" alt="iconoface" width="30" height="30"><img src="https://cdn-icons-png.flaticon.com/128/174/174855.png" alt="iconoInstagram" width="30" height="30"><img src="https://cdn-icons-png.flaticon.com/128/174/174876.png" alt="iconoTw" width="30" height="30">
+            </article>
+            <article>
+                <v-img
+                    id="logo"
+                    src="https://cdn-icons-png.flaticon.com/128/1076/1076928.png"
+                    width="50%">
+                </v-img>
                 </article>
-                    <article>
-                    <v-img
-                        src="https://cdn-icons-png.flaticon.com/128/1076/1076928.png"
-                        width="60%" alaing= justify-center
-                    ></v-img>
-                    </article>
-                <article>
-                <strong class="subheading">¡Contactenos en nuestras redes sociales!</strong>
-                <br>
-                <img src="https://cdn-icons-png.flaticon.com/128/174/174879.png" alt="iconoWatsapp" width="30" height="30"><img src="https://cdn-icons-png.flaticon.com/128/174/174848.png" alt="iconoface" width="30" height="30"><img src="https://cdn-icons-png.flaticon.com/128/174/174855.png" alt="iconoInstagram" width="30" height="30"><img src="https://cdn-icons-png.flaticon.com/128/174/174876.png" alt="iconoTw" width="30" height="30">
-                </article>
+            <article>
+                <h2 ><u class="footer-section_title">Horarios de Atensión</u></h2>
+                <p>Lunes - Viernes: 08:00am - 07:00pm</p>
+                <p>Sabado: 08:00am - 12:00pm</p>
                 
-                <v-btn
-                    v-for="icon in icons"
-                    :key="icon"
-                    class="mx-4"
-                    dark
-                    icon
-                >
-                    <v-icon size="24px">
-                    {{ icon }}
-                    </v-icon>
-                </v-btn>
-                </v-card-title>
-
-                <v-card-text class="py-2 white--text text-center">
-                {{ new Date().getFullYear() }} — <strong>Mision Tic</strong>
-                </v-card-text>
-            </v-card>
-        </v-footer>
+            </article>
+        </div>
     </v-container>
 </template>
 
@@ -57,5 +33,15 @@ export default {
 </script>
 
 <style>
+#footer{
+    background-color: #311B92;
+    display: flex;
+    justify-content: space-evenly;
+    color: white;
+    box-shadow: 5px 5px 5px #d7d4d4
+}
+#logo{
+    margin: 50%;
+}
 
 </style>
